5cc3cce927248c825b786e78b00172a3af72f61e
Home <kaymantex68@yandex.ru>
2021-01-05 10:07:42 +0300

    home

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 
 import App from './App';
 
 
 render(
-    <App /> , document.getElementById('root')
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById('root')
 )
 
---
+++ src/reducers/posts.js
@@ -0,0 +1,19 @@
+const initialState = {
+    posts: [],
+}
+
+export default function (state = initialState, action) {
+    switch (action.type) {
+        case 'SET_POSTS':
+            return {
+                ...state,
+                posts: action.payload,
+            };
+        case 'REMOVE_POSTS':
+            return {
+                ...state,
+            };
+        default:
+            return state;
+    }
+}
